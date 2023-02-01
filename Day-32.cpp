0192eb0c18f5b29c69c1c5b5fceeebfef287d981
mastershashank <beingshashank08@gmail.com>
2023-02-01 23:47:04 +0530

    Initial Commit

--- Day-32.cpp
+++ Day-32.cpp
@@ -1,20 +1,9 @@
 class Solution {
 public:
-    int bestTeamScore(vector<int>& scores, vector<int>& ages) {
-        int n = scores.size();
-        int dp[n], ans = 0;
-        vector<pair<int, int>> players;
-        for(int i = 0; i < n; i++) 
-            players.push_back({ages[i], scores[i]});
-        sort(players.begin(), players.end());
-        for(int i = 0; i < n; i++) {
-            dp[i] = players[i].second;
-            for(int j = 0; j < i; j++) {
-                if(players[j].second <= players[i].second)
-                    dp[i] = max(dp[i], dp[j] + players[i].second);
-            }
-            ans = max(ans, dp[i]);
+    string gcdOfStrings(string str1, string str2) {
+        if(str1 + str2 == str2 + str1){
+            return str1.substr(0, gcd(str1.length(), str2.length()));
         }
-        return ans;
+        return "";
     }
 };
